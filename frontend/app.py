17dfa5c2e9059ea636cda71daba34a4d651ba006
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-16 14:59:19 -0400

    heroku deploy

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, send_from_directory, request
 from flask_cors import CORS
+from sqlalchemy import create_engine
+from queries import sqlite_metrics as Q
 import os 
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/covid.gif')
+def covidGif():
+    return send_from_directory(os.path.join('..','data'), 'covid.gif')
 
+@app.route('/favicon.ico')
+def covidGif():
+    return send_from_directory('static', 'favicon.ico')
+
+    
 @app.route('/')
 def index():
     return render_template('/index.html')
