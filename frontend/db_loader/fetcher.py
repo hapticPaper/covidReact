9c5f90c3aebf3902582b890d8b45d4acb88ffc5f
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-17 18:30:41 -0400

    atlas

--- frontend/db_loader/fetcher.py
+++ frontend/db_loader/fetcher.py
@@ -19,4 +19,5 @@ def updateSqlite():
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 # def scheduled_job():
 #     print('This job is run every weekday at 5pm.')
+updateSqlite()
 sched.start()
