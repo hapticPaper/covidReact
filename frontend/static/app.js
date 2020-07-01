25ec9eef2bae7ebd13341be3ace058f479f029ec
Ian Rubenstein <ian.rubenstein@gmail.com>
2020-07-01 08:21:01 -0400

    mortality

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -3,21 +3,33 @@ function makeTable(){
     updateTotals()
     fetch('/atlasCovid').then(d=>d.json()).then(data=>{
         tbl = document.getElementById('statsTable')
-        tbl.innerHTML="<thead><tr><th>Locale</th><th class='rightd'>Confirmed</th><th class='rightd'>Deaths</th></thead>"
+        tbl.innerHTML="<thead><tr><th>Locale</th><th class='rightd'>Confirmed</th><th class='rightd'>Deaths</th><th class='rightd' style='width=200px'>Mortality Rate</th></thead>"
         data.results.forEach(city => { 
             tr = document.createElement('tr')
             td1 = document.createElement('td')
             td2 = document.createElement('td')
             td3 = document.createElement('td')
+            td4 = document.createElement('td')
             td2.setAttribute("class", "rightd");
             td3.setAttribute("class", "rightd");
+            td4.setAttribute("class", "rightd");
             td1.innerHTML= `<b>${city.locale}</b>`
             td2.innerHTML= `${city.cases.toLocaleString()}`
             td2.style.width='110px'
             td3.innerHTML= `${city.deaths.toLocaleString()}`
+            mr = city.deaths/city.cases
+            if (mr>0.05){
+
+                td4.innerHTML= `<b>${(mr).toLocaleString("en", {style: "percent"})}</b>`
+            }
+            else{
+
+                td4.innerHTML= `${(mr).toLocaleString("en", {style: "percent"})}`
+            }
             tr.appendChild(td1)
             tr.appendChild(td2)
             tr.appendChild(td3)
+            tr.appendChild(td4)
             tbl.appendChild(tr);
         });
        lr = document.getElementById('lastRefresh')
